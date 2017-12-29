8a5db1bc5ec452ebc2a8681b881c8b1bb84b28f3
langshen <langshen2008@qq.com>
2017-12-29 17:07:16 +0800

    doc

--- application/Www/Controller/DocController.class.php
+++ application/Www/Controller/DocController.class.php
@@ -1,22 +1,28 @@
 <?php
 namespace Www\Controller;
 use Spartan\Core\Controller;
+use Spartan\Lib\Document;
 
 !defined('APP_PATH') && exit('404 Not Found');
 
 class DocController extends Controller {
 
     public function index(){
-        $arrFile = \Spt::loadDirFile(APP_ROOT.'Dal',true);
-        $arrClass = [];
-        foreach ($arrFile as $key=>$value){
-            $clsTemp = new $value();
-            $arrClass[$value] = $clsTemp;
-            $arrFile[$key] .= '('.$clsTemp->strTable.')['.$clsTemp->strComment.']';
-        }
-        $this->assign('file',$arrFile);
-        print_r($arrFile);
-        $this->display();
+//        $arrFile = \Spt::loadDirFile(APP_ROOT.'Dal',true);
+//        $arrClass = [];
+//        foreach ($arrFile as $key=>$value){
+//            $clsTemp = new $value();
+//            $arrClass[$value] = $clsTemp;
+//            $arrFile[$key] .= '('.$clsTemp->strTable.')['.$clsTemp->strComment.']';
+//        }
+//        $this->assign('file',$arrFile);
+//        print_r($arrFile);
+//        $this->display();
+
+        $clsDocument = new Document(
+            ['DOC_ROOT'=>APP_ROOT.'Www']
+        );
+        $clsDocument->create();
     }
 
     public function _empty(){
