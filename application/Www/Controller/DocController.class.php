da4d687764125e9e0e5b363f3f3714b61c535cc2
langshen <langshen2008@qq.com>
2018-01-01 23:01:26 +0800

    fix

--- application/Www/Controller/DocController.class.php
+++ application/Www/Controller/DocController.class.php
@@ -6,25 +6,62 @@ use Spartan\Lib\Document;
 !defined('APP_PATH') && exit('404 Not Found');
 
 class DocController extends Controller {
+    private $config = [];
+
+    public function __construct($startSession = true){
+        parent::__construct($startSession);
+        $this->config = C('SITE');
+    }
 
     public function index(){
-//        $arrFile = \Spt::loadDirFile(APP_ROOT.'Dal',true);
-//        $arrClass = [];
+
+        $this->display('index');
+    }
+
+    /**
+     * Dal目录下的数据表信息
+     */
+    public function dal(){
+        $clsDocument = new Document(
+            ['DOC_ROOT'=>APP_ROOT.'Dal']
+        );
+        $arrResult = $clsDocument->create();
+        print_r($arrResult);
 //        foreach ($arrFile as $key=>$value){
 //            $clsTemp = new $value();
 //            $arrClass[$value] = $clsTemp;
 //            $arrFile[$key] .= '('.$clsTemp->strTable.')['.$clsTemp->strComment.']';
 //        }
-//        $this->assign('file',$arrFile);
-//        print_r($arrFile);
-//        $this->display();
-
-        $clsDocument = new Document(
-            ['DOC_ROOT'=>APP_ROOT.'Www']
-        );
-        $clsDocument->create();
+        $this->assign('config',$this->config);
+        $this->display('doc');
     }
 
+    /**
+     * Logic目录下的逻辑层方法
+     */
+    public function logic(){
+        $clsDocument = new Document(
+            ['DOC_ROOT'=>APP_ROOT.'Logic']
+        );
+        $arrResult = $clsDocument->create();
+        print_r($arrResult);
+        $this->assign('list', $arrResult);
+        $this->assign('config',$this->config);
+        $this->display('doc');
+    }
+
+    /**
+     * 当前Controller目录下，控制层的方法
+     */
+    public function controller(){
+
+
+        $this->display('doc');
+    }
+
+    /**
+     * 自定义查看其它项目Controller目录下的控制层方法
+     */
     public function _empty(){
         $strName = $this->getUrl(1);
 
