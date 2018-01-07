5b1e017636ddb619ad997f019fe0bc74c5c86647
langshen <langshen2008@qq.com>
2018-01-07 16:38:31 +0800

    fix

--- application/Www/Controller/IndexController.class.php
+++ application/Www/Controller/IndexController.class.php
@@ -6,6 +6,22 @@ use Spartan\Core\Controller;
 
 class IndexController extends Controller {
 
+    public function test(){
+        //$arrInfo = $this->Dal()->Db()->getFullFields('member_account');
+        //print_r($arrInfo);
+
+//        $arrInfo = $this->Dal()->Db()->getFullFields('system_errors');
+//        print_r($arrInfo);
+//        $strInfo = 'public $arrFields = Array('.PHP_EOL;
+//        foreach ($arrInfo as $k=>$v){
+//            $strInfo .= "   Array('{$v[0]}',{$v[1]},{$v[2]},'{$v[3]}',{$v[4]},{$v[5]},{$v[6]},'{$v[7]}','{$v[8]}'),".PHP_EOL;
+//        }
+//        $strInfo .= ');'.PHP_EOL;
+        $strInfo = $this->Dal()->Db()->showCreateTable('system_errors');
+file_put_contents('text.php',implode(PHP_EOL,$strInfo));
+        print_r($strInfo);
+    }
+
     public function index(){
         $options = Array(
             'where'=>Array(
